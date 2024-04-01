aa6b981676ff484a9376597999e2dfbfdbfe5f92
Michael Williamson <mike@zwobble.org>
2024-04-01 22:40:07 +0100

    Add busted branch drawing

--- src/views/CardsView.tsx
+++ src/views/CardsView.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import groupBy from "lodash/groupBy";
+import React from "react";
 
 import { Card } from "../app";
 import "./CardsView.scss";
@@ -47,7 +48,16 @@ function CardTreeView(props: CardTreeViewProps) {
 
   const children = cardsByParentId[card.id] || [];
 
-  // TODO: extract card row?
+  const cardHeight = 62;
+  const parentChildGap = 100;
+  const siblingGap = 12;
+  const branchStroke = "#666";
+  // TODO: account for grandchildren
+  const branchesHeight = cardHeight * children.length + (siblingGap * (children.length - 1));
+  const branchY = (childCardIndex: number) => {
+    const cardTop = (cardHeight + siblingGap) * childCardIndex;
+    return Math.floor(cardTop + cardHeight / 2) + 0.5;
+  };
 
   return (
     <div className="CardsView-TreeView">
@@ -58,17 +68,74 @@ function CardTreeView(props: CardTreeViewProps) {
           onSelect={() => onCardSelect(card.id)}
         />
       </div>
-      <div className="CardsView-TreeView-Children">
-        {children.map(childCard => (
-          <CardTreeView
-            key={childCard.id}
-            card={childCard}
-            cardsByParentId={cardsByParentId}
-            cardSelectedId={cardSelectedId}
-            onCardSelect={onCardSelect}
-          />
-        ))}
-      </div>
+      {children.length > 0 && (
+        <>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox={`0 0 ${parentChildGap} ${branchesHeight}`}
+            width={parentChildGap}
+            height={branchesHeight}
+          >
+            {children.map((childCard, childCardIndex) => {
+              const y = branchY(childCardIndex);
+
+              if (childCardIndex === 0) {
+                return (
+                  <line
+                    key={childCard.id}
+                    x1={0}
+                    y1={y}
+                    x2={parentChildGap}
+                    y2={y}
+                    stroke={branchStroke}
+                  />
+                );
+              } else if (childCardIndex === children.length - 1) {
+                return (
+                  <React.Fragment key={childCard.id}>
+                    <line
+                      x1={parentChildGap / 2}
+                      y1={y}
+                      x2={parentChildGap}
+                      y2={y}
+                      stroke={branchStroke}
+                    />
+                    <line
+                      x1={parentChildGap / 2}
+                      y1={branchY(0)}
+                      x2={parentChildGap / 2}
+                      y2={y}
+                      stroke={branchStroke}
+                    />
+                  </React.Fragment>
+                );
+              } else {
+                return (
+                  <line
+                    key={childCard.id}
+                    x1={parentChildGap / 2}
+                    y1={y}
+                    x2={parentChildGap}
+                    y2={y}
+                    stroke={branchStroke}
+                  />
+                );
+              }
+            })}
+          </svg>
+          <div className="CardsView-TreeView-Children">
+            {children.map(childCard => (
+              <CardTreeView
+                key={childCard.id}
+                card={childCard}
+                cardsByParentId={cardsByParentId}
+                cardSelectedId={cardSelectedId}
+                onCardSelect={onCardSelect}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }

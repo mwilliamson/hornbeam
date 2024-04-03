78f9faa50be15258af59ef1416f8ea915593db81
Michael Williamson <mike@zwobble.org>
2024-04-03 22:06:18 +0100

    Add card numbers

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 export interface Card {
   id: string;
+  number: number;
   parentCardId: string | null;
   text: string;
 }
@@ -17,41 +18,51 @@ export interface CardDeleteRequest {
 export class AppState {
   public readonly updateIds: ReadonlyArray<string>;
   public readonly cards: ReadonlyArray<Card>;
+  private readonly nextCardNumber: number;
 
-  public constructor(updateIds: ReadonlyArray<string>, cards: ReadonlyArray<Card>) {
+  public constructor(
+    updateIds: ReadonlyArray<string>,
+    cards: ReadonlyArray<Card>,
+    nextCardNumber: number,
+  ) {
     this.updateIds = updateIds;
     this.cards = cards;
+    this.nextCardNumber = nextCardNumber;
   }
 
   public addUpdateId(updateId: string): AppState {
     return new AppState(
       [...this.updateIds, updateId],
       this.cards,
+      this.nextCardNumber,
     );
   }
 
   public cardAdd(request: CardAddRequest): AppState {
     const card: Card = {
       id: request.id,
+      number: this.nextCardNumber,
       parentCardId: request.parentCardId,
       text: request.text,
     };
     return new AppState(
       this.updateIds,
-      [...this.cards, card]
+      [...this.cards, card],
+      this.nextCardNumber + 1,
     );
   }
 
   public cardDelete(request: CardDeleteRequest): AppState {
     return new AppState(
       this.updateIds,
-      this.cards.filter(card => card.id !== request.id)
+      this.cards.filter(card => card.id !== request.id),
+      this.nextCardNumber,
     );
   }
 }
 
 export function initialAppState(): AppState {
-  return new AppState([], []);
+  return new AppState([], [], 1);
 }
 
 export interface AppUpdate {

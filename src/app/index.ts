5633434c4d226288f5650ac292263eb56a9ddad5
Michael Williamson <mike@zwobble.org>
2024-04-15 19:14:03 +0100

    Reorder categories

--- src/app/index.ts
+++ src/app/index.ts
@@ -34,21 +34,6 @@ const categories: ReadonlyArray<Category> = [
     color: {hex: "#adf7b6"},
   },
   {
-    id: "018ec4b8-30c5-7c09-a519-4b45f141679a",
-    name: "Bug",
-    color: {hex: "#ffacbb"},
-  },
-  {
-    id: "018ec4b8-30c5-7c09-a519-4b47c6a3cb5c",
-    name: "Risk",
-    color: {hex: "#ffc09f"},
-  },
-  {
-    id: "018ec4b8-30c5-7c09-a519-4b486f2cb5c2",
-    name: "Question",
-    color: {hex: "#e6aeff"},
-  },
-  {
     id: "018ec4b8-30c5-7c09-a519-4b499ba9020c",
     name: "Feature",
     color: {hex: "#ffee93"},
@@ -57,6 +42,21 @@ const categories: ReadonlyArray<Category> = [
     id: "018ec4b8-30c5-7c09-a519-4b4a243ce8c3",
     name: "Detail",
     color: {hex: "#a0ced9"},
+  },
+  {
+    id: "018ec4b8-30c5-7c09-a519-4b486f2cb5c2",
+    name: "Question",
+    color: {hex: "#e6aeff"},
+  },
+  {
+    id: "018ec4b8-30c5-7c09-a519-4b47c6a3cb5c",
+    name: "Risk",
+    color: {hex: "#ffc09f"},
+  },
+  {
+    id: "018ec4b8-30c5-7c09-a519-4b45f141679a",
+    name: "Bug",
+    color: {hex: "#ffacbb"},
   },
 ];
 

7e74e44a31dfc2f9eb03b8365226f7cd5699ed49
Michael Williamson <mike@zwobble.org>
2024-04-19 16:58:42 +0100

    Ignore deletes from textareas

--- src/util/isInputEvent.ts
+++ src/util/isInputEvent.ts
@@ -1,4 +1,4 @@
 export default function isInputEvent(event: KeyboardEvent): boolean {
   const target = event.target as Node;
-  return target.nodeType === Node.ELEMENT_NODE && (target.nodeName === "INPUT" || target.nodeName === "SELECT");
+  return target.nodeType === Node.ELEMENT_NODE && (target.nodeName === "INPUT" || target.nodeName === "SELECT" || target.nodeName === "TEXTAREA");
 }

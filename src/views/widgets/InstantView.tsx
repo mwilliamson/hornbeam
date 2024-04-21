716f117178b7a74d46cf9338018b82021f5c486a
Michael Williamson <mike@zwobble.org>
2024-04-21 13:35:06 +0100

    Use browser datetime formatting to reduce bundle size

--- src/views/widgets/InstantView.tsx
+++ src/views/widgets/InstantView.tsx
@@ -1,16 +1,24 @@
-import { DateTimeFormatter, Instant, LocalDateTime } from "@js-joda/core";
-import { Locale }  from "@js-joda/locale_en";
+import { Instant, LocalDateTime, convert } from "@js-joda/core";
 
 interface InstantViewProps {
   value: Instant;
 }
 
-const formatter = DateTimeFormatter
-  .ofPattern("EE dd MMM yyyy HH:mm")
-  .withLocale(Locale.UK);
+const formatter = new Intl.DateTimeFormat("en-GB", {
+  weekday: "short",
+  day: "2-digit",
+  month: "short",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+});
 
 export default function InstantView(props: InstantViewProps) {
   const {value} = props;
 
-  return LocalDateTime.ofInstant(value).format(formatter);
+  const jsDate = convert(LocalDateTime.ofInstant(value)).toDate();
+
+  const parts = new Map(formatter.formatToParts(jsDate).map(part => [part.type, part.value]));
+
+  return `${parts.get("weekday")} ${parts.get("day")} ${parts.get("month")} ${parts.get("year")} ${parts.get("hour")}:${parts.get("minute")}`;
 }

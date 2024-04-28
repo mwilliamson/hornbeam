30fde3a7836fdc27aad39d623cd77820157a02f1
Michael Williamson <mike@zwobble.org>
2024-04-28 20:43:48 +0100

    Make appearance more consistent between browsers

--- src/views/widgets/ExpanderIcon.tsx
+++ src/views/widgets/ExpanderIcon.tsx
@@ -10,8 +10,8 @@ export default function ExpanderIcon(props: ExpanderIconProps) {
   const {isCollapsed} = props;
 
   return isCollapsed ? (
-    <ChevronRight className="ExpanderIcon" aria-label="Expand" />
+    <ChevronRight className="ExpanderIcon" aria-label="Expand" size={20} />
   ) : (
-    <ChevronDown className="ExpanderIcon" aria-label="Collapse" />
+    <ChevronDown className="ExpanderIcon" aria-label="Collapse" size={20} />
   );
 }

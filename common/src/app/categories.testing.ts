5528c98a347ba2ddf7b244dbfb549d1a6df9a468
Michael Williamson <mike@zwobble.org>
2024-09-04 21:06:25 +0100

    Use constant ID in categories.testing

--- common/src/app/categories.testing.ts
+++ common/src/app/categories.testing.ts
@@ -1,15 +1,15 @@
 import { Instant } from "@js-joda/core";
-import { uuidv7 } from "uuidv7";
 import { CategoryAddMutation, CategoryReorderMutation } from "./categories";
 import { presetColorWhite } from "./colors";
 
+const defaultId = "0191bea3-4895-7e56-a31e-999999999999";
 const defaultCreatedAt = Instant.ofEpochSecond(1724429942);
 
 export function testCategoryAddMutation(mutation: Partial<CategoryAddMutation>): CategoryAddMutation {
   return {
     color: {presetColorId: presetColorWhite.id},
     createdAt: defaultCreatedAt,
-    id: uuidv7(),
+    id: defaultId,
     name: "<default test name>",
     ...mutation,
   };

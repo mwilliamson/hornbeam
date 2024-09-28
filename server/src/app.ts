eac0b6be8c933ff54dc6735ec68e520ff01b5666
Michael Williamson <mike@zwobble.org>
2024-09-28 12:21:31 +0100

    Extract app and app transaction for server

---
+++ server/src/app.ts
@@ -0,0 +1,167 @@
+import { Transaction } from "kysely";
+import { uuidv7 } from "uuidv7";
+import { Database } from "./database";
+import { DB } from "./database/types";
+import { AppMutation } from "hornbeam-common/lib/app/snapshots";
+import { MutationLogRepositoryDatabase } from "./repositories/mutationLog";
+import { CategoryRepositoryDatabase } from "./repositories/categories";
+import { CommentRepositoryDatabase } from "./repositories/comments";
+import { ProjectRepositoryDatabase } from "./repositories/projects";
+import { handleNever } from "hornbeam-common/lib/util/assertNever";
+import { CardRepositoryDatabase } from "./repositories/cards";
+import { serializeAllCategoriesResponse, serializeAllColorsResponse, serializeBoardCardTreesResponse, serializeCardChildCountResponse, serializeCardHistoryResponse, serializeCardResponse, serializeParentBoardResponse, serializeParentCardResponse, serializeSearchCardsResponse, ServerQuery } from "hornbeam-common/lib/serialization/serverQueries";
+import mapSeries from "p-map-series";
+import { colorSetPresetsOnly } from "hornbeam-common/lib/app/colors";
+import { CardHistoryFetcher } from "./repositories/cardHistory";
+
+export class App {
+  private readonly database: Database;
+
+  constructor(database: Database) {
+    this.database = database;
+  }
+
+  public async transaction<T>(f: (transaction: AppTransaction) => Promise<T>): Promise<T> {
+    return this.database.transaction().execute(transaction => {
+      const appTransaction = new AppTransaction(transaction);
+      return f(appTransaction);
+    });
+  }
+}
+
+class AppTransaction {
+  private readonly transaction: Transaction<DB>;
+
+  constructor(transaction: Transaction<DB>) {
+    this.transaction = transaction;
+  }
+
+  public async query(serverQueries: ReadonlyArray<ServerQuery>) {
+    const mutationLogRepository = new MutationLogRepositoryDatabase(this.transaction);
+    const latestIndex = await mutationLogRepository.fetchLatestIndex();
+    return {
+      latestIndex,
+      queryResults: await this.executeQueries(serverQueries),
+    };
+  }
+
+  private async executeQueries(
+    serverQueries: ReadonlyArray<ServerQuery>,
+  ) {
+    return await mapSeries(serverQueries, async serverQuery => {
+      switch (serverQuery.type) {
+        case "card": {
+          const cardRepository = new CardRepositoryDatabase(this.transaction);
+          const result = await cardRepository.fetchById(serverQuery.cardId);
+          return serializeCardResponse(result);
+        }
+        case "parentCard": {
+          const cardRepository = new CardRepositoryDatabase(this.transaction);
+          const result = await cardRepository.fetchParentByChildId(serverQuery.cardId);
+          return serializeParentCardResponse(result);
+        }
+        case "cardChildCount": {
+          const cardRepository = new CardRepositoryDatabase(this.transaction);
+          const result = await cardRepository.fetchChildCountByParentId(serverQuery.cardId);
+          return serializeCardChildCountResponse(result);
+        }
+        case "cardHistory": {
+          const cardRepository = new CardRepositoryDatabase(this.transaction);
+          const commentRepository = new CommentRepositoryDatabase(this.transaction);
+          const cardHistoryFetcher = new CardHistoryFetcher(cardRepository, commentRepository);
+          const cardHistory = await cardHistoryFetcher.fetchCardHistoryById(serverQuery.cardId);
+          return serializeCardHistoryResponse(cardHistory);
+        }
+        case "searchCards": {
+          const cardRepository = new CardRepositoryDatabase(this.transaction);
+          const result = await cardRepository.search(serverQuery.searchTerm);
+          return serializeSearchCardsResponse(result);
+        }
+        case "boardCardTrees": {
+          const cardRepository = new CardRepositoryDatabase(this.transaction);
+          const result = await cardRepository.fetchBoardCardTrees(
+            serverQuery.boardId,
+            new Set(serverQuery.cardStatuses),
+          );
+          return serializeBoardCardTreesResponse(result);
+        }
+        case "parentBoard": {
+          const cardRepository = new CardRepositoryDatabase(this.transaction);
+          const result = await cardRepository.fetchParentBoard(serverQuery.boardId);
+          return serializeParentBoardResponse(result);
+        }
+        case "allCategories": {
+          const categoryRepository = new CategoryRepositoryDatabase(this.transaction);
+          const result = await categoryRepository.fetchAll();
+          return serializeAllCategoriesResponse(result);
+        }
+        case "allColors": {
+          return serializeAllColorsResponse(colorSetPresetsOnly.allPresetColors());
+        }
+        default: {
+          handleNever(serverQuery, null);
+        }
+      }
+    });
+  }
+
+  public async mutate(
+    mutation: AppMutation,
+  ): Promise<number> {
+    const mutationLogRepository = new MutationLogRepositoryDatabase(this.transaction);
+    const index = await mutationLogRepository.add(uuidv7(), mutation);
+
+    await this.applyMutation(mutation);
+
+    return index;
+  }
+
+  private async applyMutation(
+    mutation: AppMutation,
+  ): Promise<void> {
+    switch (mutation.type) {
+      case "cardAdd": {
+        const cardRepository = new CardRepositoryDatabase(this.transaction);
+        await cardRepository.add(mutation.cardAdd);
+        return;
+      }
+      case "cardEdit": {
+        const cardRepository = new CardRepositoryDatabase(this.transaction);
+        await cardRepository.update(mutation.cardEdit);
+        return;
+      }
+      case "cardMove": {
+        throw new Error("cardMove not supported");
+      }
+      case "cardMoveToAfter": {
+        throw new Error("cardMoveToAfter not supported");
+      }
+      case "cardMoveToBefore": {
+        throw new Error("cardMoveToBefore not supported");
+      }
+      case "categoryAdd": {
+        const categoryRepository = new CategoryRepositoryDatabase(this.transaction);
+        await categoryRepository.add(mutation.categoryAdd);
+        return;
+      }
+      case "categoryReorder": {
+        const categoryRepository = new CategoryRepositoryDatabase(this.transaction);
+        await categoryRepository.reorder(mutation.categoryReorder);
+        return;
+      }
+      case "commentAdd": {
+        const commentRepository = new CommentRepositoryDatabase(this.transaction);
+        await commentRepository.add(mutation.commentAdd);
+        return;
+      }
+      case "projectAdd": {
+        const projectRepository = new ProjectRepositoryDatabase(this.transaction);
+        await projectRepository.add(mutation.projectAdd);
+        return;
+      }
+      default: {
+        return handleNever(mutation, undefined);
+      }
+    }
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,11 @@
 import "disposablestack/auto";
 import { isLeft } from "fp-ts/lib/Either";
-import mapSeries from "p-map-series";
 import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
-import { uuidv7 } from "uuidv7";
 import path from "node:path";
 import { QueryRequestBody, QueryResponseBody, UpdateRequestBody, UpdateResponseBody } from "hornbeam-common/lib/serialization/serverApi";
-import {serializeAllCategoriesResponse, serializeAllColorsResponse, serializeBoardCardTreesResponse, serializeCardChildCountResponse, serializeCardHistoryResponse, serializeCardResponse, serializeParentBoardResponse, serializeParentCardResponse, serializeSearchCardsResponse, ServerQuery} from "hornbeam-common/lib/serialization/serverQueries";
-import { handleNever } from "hornbeam-common/lib/util/assertNever";
 import { databaseConnect } from "./database";
-import { CardRepositoryDatabase } from "./repositories/cards";
-import { CategoryRepositoryDatabase } from "./repositories/categories";
-import { colorSetPresetsOnly } from "hornbeam-common/lib/app/colors";
-import { AppMutation } from "hornbeam-common/lib/app/snapshots";
-import { DB } from "./database/types";
-import { Transaction } from "kysely";
-import { CardHistoryFetcher } from "./repositories/cardHistory";
-import { CommentRepositoryDatabase } from "./repositories/comments";
-import { MutationLogRepositoryDatabase } from "./repositories/mutationLog";
-import { ProjectRepositoryDatabase } from "./repositories/projects";
+import { App } from "./app";
 
 interface ServerOptions {
   databaseUrl: string;
@@ -39,6 +26,8 @@ export async function startServer({databaseUrl, port}: ServerOptions): Promise<S
     await database.destroy();
   });
 
+  const app = new App(database);
+
   const fastify = Fastify({
     logger: true,
   });
@@ -56,13 +45,10 @@ export async function startServer({databaseUrl, port}: ServerOptions): Promise<S
 
     const serverQueries = bodyResult.right.queries;
 
-    const {latestIndex, queryResults} = await database.transaction().execute(async transaction => {
-      const mutationLogRepository = new MutationLogRepositoryDatabase(transaction);
-      const latestIndex = await mutationLogRepository.fetchLatestIndex();
-      return {
-        latestIndex,
-        queryResults: await executeQueries(transaction, serverQueries),
-      };
+    const app = new App(database);
+
+    const {latestIndex, queryResults} = await app.transaction(async transaction => {
+      return await transaction.query(serverQueries);
     });
 
     return QueryResponseBody.encode({
@@ -70,67 +56,6 @@ export async function startServer({databaseUrl, port}: ServerOptions): Promise<S
       results: queryResults,
     });
   });
-
-  const executeQueries = async (
-    transaction: Transaction<DB>,
-    serverQueries: ReadonlyArray<ServerQuery>
-  ) => {
-    return await mapSeries(serverQueries, async serverQuery => {
-      switch (serverQuery.type) {
-        case "card": {
-          const cardRepository = new CardRepositoryDatabase(transaction);
-          const result = await cardRepository.fetchById(serverQuery.cardId);
-          return serializeCardResponse(result);
-        }
-        case "parentCard": {
-          const cardRepository = new CardRepositoryDatabase(transaction);
-          const result = await cardRepository.fetchParentByChildId(serverQuery.cardId);
-          return serializeParentCardResponse(result);
-        }
-        case "cardChildCount": {
-          const cardRepository = new CardRepositoryDatabase(transaction);
-          const result = await cardRepository.fetchChildCountByParentId(serverQuery.cardId);
-          return serializeCardChildCountResponse(result);
-        }
-        case "cardHistory": {
-          const cardRepository = new CardRepositoryDatabase(transaction);
-          const commentRepository = new CommentRepositoryDatabase(transaction);
-          const cardHistoryFetcher = new CardHistoryFetcher(cardRepository, commentRepository);
-          const cardHistory = await cardHistoryFetcher.fetchCardHistoryById(serverQuery.cardId);
-          return serializeCardHistoryResponse(cardHistory);
-        }
-        case "searchCards": {
-          const cardRepository = new CardRepositoryDatabase(transaction);
-          const result = await cardRepository.search(serverQuery.searchTerm);
-          return serializeSearchCardsResponse(result);
-        }
-        case "boardCardTrees": {
-          const cardRepository = new CardRepositoryDatabase(transaction);
-          const result = await cardRepository.fetchBoardCardTrees(
-            serverQuery.boardId,
-            new Set(serverQuery.cardStatuses),
-          );
-          return serializeBoardCardTreesResponse(result);
-        }
-        case "parentBoard": {
-          const cardRepository = new CardRepositoryDatabase(transaction);
-          const result = await cardRepository.fetchParentBoard(serverQuery.boardId);
-          return serializeParentBoardResponse(result);
-        }
-        case "allCategories": {
-          const categoryRepository = new CategoryRepositoryDatabase(transaction);
-          const result = await categoryRepository.fetchAll();
-          return serializeAllCategoriesResponse(result);
-        }
-        case "allColors": {
-          return serializeAllColorsResponse(colorSetPresetsOnly.allPresetColors());
-        }
-        default: {
-          handleNever(serverQuery, null);
-        }
-      }
-    });
-  };
 
   fastify.post("/update", async (request, reply) => {
     const bodyResult = UpdateRequestBody.decode(request.body);
@@ -140,76 +65,14 @@ export async function startServer({databaseUrl, port}: ServerOptions): Promise<S
 
     const update = bodyResult.right.update;
 
-    const index = await database.transaction().execute(async transaction => {
-      return await mutate(transaction, update.mutation);
+    const index = await app.transaction(async transaction => {
+      return await transaction.mutate(update.mutation);
     });
 
     return UpdateResponseBody.encode({
       snapshotIndex: index,
     });
   });
-
-  async function mutate(
-    transaction: Transaction<DB>,
-    mutation: AppMutation,
-  ): Promise<number> {
-    const mutationLogRepository = new MutationLogRepositoryDatabase(transaction);
-    const index = await mutationLogRepository.add(uuidv7(), mutation);
-
-    await applyMutation(transaction, mutation);
-
-    return index;
-  }
-
-  async function applyMutation(
-    transaction: Transaction<DB>,
-    mutation: AppMutation,
-  ): Promise<void> {
-    switch (mutation.type) {
-      case "cardAdd": {
-        const cardRepository = new CardRepositoryDatabase(transaction);
-        await cardRepository.add(mutation.cardAdd);
-        return;
-      }
-      case "cardEdit": {
-        const cardRepository = new CardRepositoryDatabase(transaction);
-        await cardRepository.update(mutation.cardEdit);
-        return;
-      }
-      case "cardMove": {
-        throw new Error("cardMove not supported");
-      }
-      case "cardMoveToAfter": {
-        throw new Error("cardMoveToAfter not supported");
-      }
-      case "cardMoveToBefore": {
-        throw new Error("cardMoveToBefore not supported");
-      }
-      case "categoryAdd": {
-        const categoryRepository = new CategoryRepositoryDatabase(transaction);
-        await categoryRepository.add(mutation.categoryAdd);
-        return;
-      }
-      case "categoryReorder": {
-        const categoryRepository = new CategoryRepositoryDatabase(transaction);
-        await categoryRepository.reorder(mutation.categoryReorder);
-        return;
-      }
-      case "commentAdd": {
-        const commentRepository = new CommentRepositoryDatabase(transaction);
-        await commentRepository.add(mutation.commentAdd);
-        return;
-      }
-      case "projectAdd": {
-        const projectRepository = new ProjectRepositoryDatabase(transaction);
-        await projectRepository.add(mutation.projectAdd);
-        return;
-      }
-      default: {
-        return handleNever(mutation, undefined);
-      }
-    }
-  }
 
   try {
     await fastify.listen({ port });
